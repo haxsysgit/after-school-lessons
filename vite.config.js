484e5edc01da3133a474657fa9140be3b757c617
haxsysgit <elenasuluarinze@gmail.com>
2025-10-22 01:06:59 +0100

    Deploy build to GitHub Pages

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  base: '/after-school-lessons/',   // IMPORTANT FOR GITHUB PAGES
-  plugins: [vue()]
-})
